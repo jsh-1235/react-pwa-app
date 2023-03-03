645e72cf14b6303f20cd5e772401dcbc7c28f6e2
jsh-1235 <jsh-1235@hanmail.net>
2023-03-04 01:31:04 +0900

    update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-import React from "react";
+import React, { Suspense } from "react";
+import { useState, useEffect } from "react";
 
-import "./App.css";
+import { Route, Routes, Navigate, useNavigate, useLocation } from "react-router-dom";
 
-function App() {
-  return <div className="App">This is a app.</div>;
+const Intro = React.lazy(() => import("./pages/intro/Intro"));
+const Home = React.lazy(() => import("./pages/home/Home"));
+const NotFound = React.lazy(() => import("./pages/NotFound"));
+
+export default function App() {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  useEffect(() => {
+    console.group("App");
+    console.log(location.pathname);
+    console.groupEnd("App");
+
+    // if (location.pathname === "/") navigate("/intro");
+  }, []);
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/" element={<Home title="Home" />} />
+        <Route path="/intro" element={<Intro title="Intro" />} />
+        <Route path="/none" element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Suspense>
+  );
 }
-
-export default App;
---
+++ src/pages/home/Home.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+
+import styles from "./Home.module.css";
+import "../../Components.css";
+import { AiFillCloseCircle } from "react-icons/ai";
+
+import { useEffect } from "react";
+
+export default function Home() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const handleTabClose = (event) => {
+      event.preventDefault();
+
+      console.log("beforeunload event triggered");
+
+      return (event.returnValue = "Are you sure you want to exit?");
+    };
+
+    window.addEventListener("beforeunload", handleTabClose);
+
+    return () => {
+      window.removeEventListener("beforeunload", handleTabClose);
+    };
+  }, []);
+
+  const handleClose = (e) => {
+    e.preventDefault();
+
+    window.open("about:blank", "_self");
+
+    window.close();
+  };
+
+  return (
+    <div className={styles.container}>
+      <header className={styles.header}>제품 선택</header>
+      <main className={styles.menu}>
+        <div className={styles.buttonStyle}>
+          <div className="button">
+            <div className="button" onClick={(e) => navigate("/main")}>
+              BT-ACTB_LITE
+            </div>
+          </div>
+        </div>
+        <div className={styles.buttonStyle}>
+          <div className="button" onClick={(e) => navigate("/intro")}>
+            BT-ACTB
+          </div>
+        </div>
+      </main>
+      <footer className={styles.footer}>
+        <AiFillCloseCircle className="image-button" size="64" onClick={handleClose} />
+      </footer>
+    </div>
+  );
+}
